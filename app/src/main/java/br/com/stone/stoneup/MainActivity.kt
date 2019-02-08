ffaa0f5548494958ba901d4f6acb7ac83b017424
Lucas Albuquerque <lucas.albuquerque12@gmail.com>
2019-02-08 21:42:41 -0200

    Adds receipt layout

--- app/src/main/java/br/com/stone/stoneup/MainActivity.kt
+++ app/src/main/java/br/com/stone/stoneup/MainActivity.kt
@@ -1,10 +1,15 @@
 package br.com.stone.stoneup
 
+import android.content.Context
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import br.com.stone.pay.core.PAL
 import kotlinx.android.synthetic.main.activity_main.*
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     private val printerProvider by lazy {
@@ -18,6 +23,17 @@ class MainActivity : AppCompatActivity() {
         val animation = AnimationUtils.loadAnimation(this, R.anim.animation_bounce)
         arrowDownImageView.startAnimation(animation)
 //        printerProvider.print()
+
+        val receiptLayout = findViewById<ConstraintLayout>(R.id.receiptLayout)
+
+//        findViewById<ImageView>(R.id.imagePreviewContainer).setImageBitmap(loadBitmapFromView(receiptLayout))
+    }
+
+    fun generateBitmapFrom(view: View): Bitmap? {
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.layout_receipt, null)
+
+        return null
     }
 }
 
