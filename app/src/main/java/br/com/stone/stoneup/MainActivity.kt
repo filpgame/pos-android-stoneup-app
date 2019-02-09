8855c04670390b76ec7d53253c835b931d249e26
Felipe Rodrigues <filpgame@gmail.com>
2019-02-09 03:31:21 -0200

    camera on card inserted

--- app/src/main/java/br/com/stone/stoneup/MainActivity.kt
+++ app/src/main/java/br/com/stone/stoneup/MainActivity.kt
@@ -4,27 +4,30 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.view.animation.Interpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.drawable.toBitmap
 import br.com.stone.pay.core.PAL
 import br.com.stone.payment.domain.datamodel.CardInfo
 import br.com.stone.payment.domain.datamodel.Result
 import br.com.stone.payment.domain.exception.PalException
 import br.com.stone.payment.domain.interfaces.DetectCardListener
 import br.com.stone.payment.domain.interfaces.ReadCardInfoListener
+import br.com.stone.poladroid.animationListener
 import br.com.stone.poladroid.main.MainContract
 import br.com.stone.poladroid.printer.IngenicoAdapter
 import br.com.stone.poladroid.printer.PAXAdapter
 import br.com.stone.poladroid.printer.VoidAdapter
+import br.com.stone.poladroid.show
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
@@ -55,6 +58,34 @@ class MainActivity : AppCompatActivity(), MainContract.View, DetectCardListener,
         val animation = AnimationUtils.loadAnimation(this, R.anim.animation_bounce)
         arrowDownImageView.startAnimation(animation)
 //        val receiptLayout = findViewById<ConstraintLayout>(R.id.receiptLayout)
+        insertCardTextView.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun hideWelcome() {
+        val welcomeAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_slidedown_fadein)
+        welcomeAnimation.animationListener { start { welcomeTextView.show() } }
+        welcomeAnimation.fillAfter = true
+        welcomeAnimation.interpolator = Interpolator {
+            Math.abs(it - 1f)
+        }
+
+        val stoneUpAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_slideup)
+        stoneUpAnimation.fillAfter = true
+        stoneUpAnimation.interpolator = Interpolator {
+            Math.abs(it - 1f)
+        }
+        welcomeTextView.startAnimation(welcomeAnimation)
+        stoneUpImageView.startAnimation(stoneUpAnimation)
+        insertCardTextView.show()
+        arrowDownImageView.show()
+    }
+
+    fun openCamera() {
+        startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra("android.intent.extras.CAMERA_FACING", 1)
+        }, REQUEST_CAMERA)
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -62,26 +93,27 @@ class MainActivity : AppCompatActivity(), MainContract.View, DetectCardListener,
             when (requestCode) {
                 REQUEST_CAMERA -> {
                     val photo = data?.extras?.get("data") as Bitmap
-                    presenter.printPicture(photo)
+
+                    val stoneup = BitmapFactory.decodeResource(resources, R.drawable.ic_stoneup_top)
+                    val bottom = BitmapFactory.decodeResource(resources, R.drawable.ic_stoneup_bottom)
+                    presenter.printPictures(stoneup, photo, bottom)
                 }
                 else -> toast("Unknown request")
             }
         }
         val receiptLayout = findViewById<ConstraintLayout>(R.id.receiptLayout)
-
 //        findViewById<ImageView>(R.id.imagePreviewContainer).setImageBitmap(loadBitmapFromView(receiptLayout))
-
-        PalHelper.tryInitializePal(this)
+//        PalHelper.tryInitializePal(this)
     }
 
     override fun onResume() {
         super.onResume()
-        Handler(Looper.getMainLooper()).postDelayed({ startCardDetection() }, 1500)
+//        Handler(Looper.getMainLooper()).postDelayed({ startCardDetection() }, 1500)
     }
 
     override fun onPause() {
         super.onPause()
-        PalHelper.stopCardDetection()
+//        PalHelper.stopCardDetection()
     }
 
     fun generateBitmapFrom(view: View): Bitmap? {
@@ -97,9 +129,7 @@ class MainActivity : AppCompatActivity(), MainContract.View, DetectCardListener,
     }
 
     override fun onIccInserted() {
-        startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
-            putExtra("android.intent.extras.CAMERA_FACING", 1)
-        }, REQUEST_CAMERA)
+
     }
 
     override fun onCancelledDetection() {
